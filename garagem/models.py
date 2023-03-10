7b88bcffc9826458fa8c58b45e08970dde83dfa5
Felipe Rotermel <feliperotermell@gmail.com>
2023-03-10 16:23:53 -0300

    Criação da marca nos "models.py"

--- garagem/models.py
+++ garagem/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nascionalidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nome.upper()} ({self.id}) "
