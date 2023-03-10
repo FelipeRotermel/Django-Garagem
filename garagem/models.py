aeb9f7b044cea7e9765e345ad0029ea3ecad63f3
Felipe Rotermel <feliperotermell@gmail.com>
2023-03-10 16:33:14 -0300

    Criação da categorias no "models.py"

--- garagem/models.py
+++ garagem/models.py
@@ -6,3 +6,9 @@ class Marca(models.Model):
 
     def __str__(self):
         return f"{self.nome.upper()} ({self.id}) "
+    
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id}) "
