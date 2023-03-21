e2cceca62893c0b6b91d08b519c4878a536d220a
Felipe Rotermel <feliperotermell@gmail.com>
2023-03-21 15:14:20 -0300

    Criação de cor e acessorios no "models.py"

--- garagem/admin.py
+++ garagem/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import Marca
 from .models import Categoria
 from .models import Veiculo
+from .models import Acessorio
+from .models import Cor
 
 admin.site.register(Marca)
-
 admin.site.register(Categoria)
-
 admin.site.register(Veiculo)
+admin.site.register(Acessorio)
+admin.site.register(Cor)
--- garagem/models.py
+++ garagem/models.py
@@ -13,13 +13,29 @@ class Categoria(models.Model):
     def __str__(self):
         return f"{self.descricao} ({self.id}) "
 
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id}) "
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id}) "
+    
+    class Meta:
+        verbose_name_plural = "Cores"
+
 class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     modelo = models.CharField(max_length=50)
     ano = models.IntegerField()
-    cor = models.CharField(max_length=50)
-    preco = models.DecimalField(max_digits=10, decimal_places=3)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    placa = models.CharField(max_length=10, blank=True, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=3, default=0, blank=True, null=True)
 
     def __str__(self):
         return f"{self.modelo} ({self.id}) "
