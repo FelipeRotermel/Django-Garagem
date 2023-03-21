4f59af450976966709dfcce4686026dc704e0cb6
Felipe Rotermel <feliperotermell@gmail.com>
2023-03-21 14:13:10 -0300

    Criação do veiculos no "models.py"

--- garagem/admin.py
+++ garagem/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from .models import Marca
 from .models import Categoria
+from .models import Veiculo
 
 admin.site.register(Marca)
 
 admin.site.register(Categoria)
+
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nascionalidade = models.CharField(max_length=50)
+    nascionalidade = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome.upper()} ({self.id}) "
@@ -12,3 +12,14 @@ class Categoria(models.Model):
 
     def __str__(self):
         return f"{self.descricao} ({self.id}) "
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    modelo = models.CharField(max_length=50)
+    ano = models.IntegerField()
+    cor = models.CharField(max_length=50)
+    preco = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self):
+        return f"{self.modelo} ({self.id}) "
